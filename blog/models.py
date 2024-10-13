21abede9b324b37ed2b54c1fce113e8458880022
xiaoyouyou3 <xiaolinyousi3@gmail.com>
2024-10-13 22:41:13 +0900

    Modified templates to display posts from datebase.

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
-    def publush(self):
+    def publish(self):
         self.published_date = timezone.now()
         self.save()
     
